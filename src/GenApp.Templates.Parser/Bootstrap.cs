cc1f368bb3365ef43e35625b8b8e6a3a559513d9
Denys Spektrov <spektrovpe@gmail.com>
2024-02-25 22:26:57 +0200

    Add templates parser

---
+++ src/GenApp.Templates.Parser/Bootstrap.cs
@@ -0,0 +1,14 @@
+﻿using GenApp.Templates.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GenApp.Templates.Parser;
+
+public static class Bootstrap
+{
+    public static IServiceCollection AddTemplateParser(this IServiceCollection services)
+    {
+        services.AddScoped<ITemplateParser, TemplateParser>();
+
+        return services;
+    }
+}
---
+++ src/GenApp.Templates.Parser/TemplateParser.cs
@@ -0,0 +1,34 @@
+﻿using GenApp.Templates.Abstractions;
+using RazorEngineCore;
+
+namespace GenApp.Templates.Parser;
+
+public class TemplateParser : ITemplateParser
+{
+    public async Task<string> ParseAsync<T>(T model, CancellationToken token)
+        where T : BaseTemplateModel
+    {
+        var templatePath = GetTemplatePath(model.TemplateName);
+
+        if (!File.Exists(templatePath))
+        {
+            throw new FileNotFoundException($"Template not found at '{templatePath}'.");
+        }
+
+        var templateContent = await File.ReadAllTextAsync(templatePath, token);
+
+        var razorEngine = new RazorEngine();
+        var template = await razorEngine.CompileAsync(templateContent);
+        var parsedContent = await template.RunAsync(model);
+
+        return parsedContent;
+    }
+
+    private static string GetTemplatePath(string templateName)
+    {
+        var templatesDirectory = @"..\GenApi.Templates\Templates";
+        var templateFile = $"{templateName}.cshtml";
+
+        return Path.Combine(templatesDirectory, templateFile);
+    }
+}
