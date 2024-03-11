eb0189969905ee50df6943bc58dcbfde168a6a84
Denys Spektrov <spektrovpe@gmail.com>
2024-03-11 22:57:30 +0200

    Add generate of custom migrator

---
+++ src/GenApp.DomainServices/CommandHandlers/CustomMigrationsGenCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Compression;
+using GenApp.Domain.Interfaces;
+using GenApp.Domain.Models;
+using GenApp.DomainServices.Extensions;
+using GenApp.Templates.Resources.Models;
+
+namespace GenApp.DomainServices.CommandHandlers;
+internal class CustomMigrationsGenCommand(IFileGenService fileGenService) : IGenCommand
+{
+    public async Task ExecuteAsync(ZipArchive archive, ApplicationDataModel model, CancellationToken token)
+    {
+        await fileGenService.CreateEntryAsync(
+            archive,
+            "CustomMigrations/CustomDbMigrator.cs".ToDalProjectFile(model.AppName),
+            new CustomMigrationsClassModel
+            {
+                Namespace = $"{model.AppName}.DAL.CustomMigrations",
+            },
+            token);
+
+        await fileGenService.CreateEntryAsync(
+            archive,
+            "CustomMigrations/ICustomDbMigrator.cs".ToDalProjectFile(model.AppName),
+            new CustomMigrationsInterfaceModel
+            {
+                Namespace = $"{model.AppName}.DAL.CustomMigrations",
+            },
+            token);
+    }
+}
--- src/GenApp.Templates.Resources/TemplateNames.cs
+++ src/GenApp.Templates.Resources/TemplateNames.cs
@@ -7,4 +7,8 @@ public static class TemplateNames
     public const string IBaseEntity = nameof(IBaseEntity);
 
     public const string CsprojFile = nameof(CsprojFile);
+
+    public const string CustomMigrationsClass = nameof(CustomMigrationsClass);
+
+    public const string CustomMigrationsInterface = nameof(CustomMigrationsInterface);
 }
