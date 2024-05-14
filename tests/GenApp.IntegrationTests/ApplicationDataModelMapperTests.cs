161cfb1037e013fedfcaac2f7e9679fa295b9ae1
Denys Spektrov <spektrovpe@gmail.com>
2024-05-14 15:28:11 +0300

    setup integration and parsers.Csharp test projects

---
+++ tests/GenApp.IntegrationTests/ApplicationDataModelMapperTests.cs
@@ -0,0 +1,4 @@
+﻿namespace GenApp.IntegrationTests;
+public class ApplicationDataModelMapperTests
+{
+}
---
+++ tests/GenApp.Parsers.CSharp.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace GenApp.Parsers.CSharp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
